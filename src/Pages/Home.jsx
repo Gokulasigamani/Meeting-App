a172d536f529c8e4b9be56b129cf174949b4a4c6
Gokulasigamani <113229521+Gokulasigamani@users.noreply.github.com>
2024-04-10 18:58:56 +0530

    fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import { useState,useEffect } from "react";
 import Card from "../Components/Card";
+import { useNavigate } from 'react-router-dom';
 
 function Home()
 {       
     let options={weekday:'long',year:'numeric',month:'long',day:'numeric'}
 
-      let [Live,SetLive]=useState([])
-      
+      let [Live,SetLive]=useState([]);
+      let[filter,setfilter]=useState([]);
+     
       useEffect(()=>{
         
         fetch('https://sunajo-85479-default-rtdb.europe-west1.firebasedatabase.app/meetings.json').
@@ -30,7 +32,38 @@ function Home()
     })
 
     },[])
-
+     
+    let filtermeeting=Live;
+    let Navigate=useNavigate();
+    if(filter=='dev')
+    {
+        filtermeeting=Live.filter(meeting => meeting.type=='dev')
+    }
+    else if(filter=='operation')
+    {
+        filtermeeting=Live.filter(meeting => meeting.type=='operation')
+    }
+    else if(filter=='marketing')
+    {
+        filtermeeting=Live.filter(meeting => meeting.type=='marketing')
+    }
+    
+    function devHandler()
+    {
+        setfilter('dev');
+    }
+    function opHandler()
+    {
+        setfilter('operation');
+    }
+    function maHandler()
+    {
+        setfilter('marketing');
+    }
+    function addmeet()
+    {
+        Navigate('/meet')
+    }
     
     return(
         <>
@@ -39,24 +72,25 @@ function Home()
             <div className="link text-blue-500 text-5xl font-bold">Zoon.com</div>
 
             <div className="gap-5 flex">
-                <button className="bg-blue-200 px-6 font-semibold py-2 rounded-[150px] hover:bg-black hover:text-white transition-all shadow-md ">Add Meetings</button>
+                <button onClick={addmeet} className="bg-blue-200 px-6 font-semibold py-2 rounded-[150px] hover:bg-black hover:text-white transition-all shadow-md ">Add Meetings</button>
                 <button className="font-bold text-blue-500">My Meetings</button>
             </div>
         </nav>
 
             <div className="w-[1150px] m-auto mt-32 flex gap-10">
-                <button className="bg-orange-100 text-orange-400 px-8 font-semibold py-3 rounded-[10px] transition-all shadow-md">DEVELOPERS</button>
-                <button className="bg-orange-100 text-orange-400 px-8 font-semibold py-3 rounded-[10px] transition-all shadow-md">OPERATIONS</button>
-                <button className="bg-orange-100 text-orange-400 px-8 font-semibold py-3 rounded-[10px] transition-all shadow-md">MARKETING</button>
+                <button onClick={devHandler} className="bg-orange-100 text-orange-400 px-8 font-semibold py-3 rounded-[10px] transition-all shadow-md">DEVELOPERS</button>
+                <button onClick={opHandler} className="bg-orange-100 text-orange-400 px-8 font-semibold py-3 rounded-[10px] transition-all shadow-md">OPERATIONS</button>
+                <button onClick={maHandler} className="bg-orange-100 text-orange-400 px-8 font-semibold py-3 rounded-[10px] transition-all shadow-md">MARKETING</button>
                 
             </div>
 
            
 
-              <div className="flex font-semibold gap-2 w-[1150px] mt-20 m-auto flex-wrap">
-                
+              <div className="flex font-semibold gap-2 w-[1150px] mt-20 m-auto flex-wrap">  
               {
-                Live.map((LiveDatas)=>{
+                 filtermeeting.length >0 ?
+
+                filtermeeting.map((LiveDatas)=>{
 
                     let date=new Date(LiveDatas.date)
 
@@ -64,6 +98,10 @@ function Home()
                     
                       return <Card title={LiveDatas.topic} date={fdate} type={LiveDatas.type} />  
                 })
+
+                :
+                <div className="font-serif bottom-12  text-2xl"> No Meeting Available..  </div>
+
              }
               </div>
 
